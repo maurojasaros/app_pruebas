9868059eb04e912451fe0c8af83dc21cf42e8179
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2024-11-01 23:21:52 -0300

    sigo configurando las pages de las categorias, estoy agregando juegos a cada categoria, de modo que aparezca una pequeña descripcion, imagen, y precio para cada juego

--- src/app/deportes/deportes.page.ts
+++ src/app/deportes/deportes.page.ts
@@ -7,16 +7,32 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['./deportes.page.scss'],
 })
 export class DeportesPage implements OnInit {
+  // Array de juegos
+  juegos: { titulo: string; precio: number; descripcion: string; imagen: string }[] = [
+    {
+      titulo: 'FIFA 2023',
+      precio: 39990,
+      descripcion: 'Disfruta de la experiencia más realista del fútbol con FIFA 2023, donde cada partido cuenta y cada jugador tiene su propio estilo.',
+      imagen: 'assets/img/Fifa 2023.jpg' // Asegúrate de que la imagen esté en esta ruta
+    },
+    {
+      titulo: 'UFC',
+      precio: 39990,
+      descripcion: 'Enfréntate a los mejores luchadores del mundo en UFC. Experimenta la emoción de las artes marciales mixtas como nunca antes.',
+      imagen: 'assets/img/ufc.jpg' // Asegúrate de que la imagen esté en esta ruta
+    }
+  ];
 
-  constructor(
-    private menu: MenuController
-  ) { }
+  constructor(private menu: MenuController) { }
 
-  
   ngOnInit() {
   }
 
   abrirMenu() {
     this.menu.open('mainMenu');
+  }
+
+  agregarAlCarrito(juego: any) {
+    console.log(`Juego añadido al carrito: ${juego.titulo}`);
   }
 }
