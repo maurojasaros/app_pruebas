16b1fad18aeb82d172fea9dc1743170a6143149d
maurojasaros <114428046+maurojasaros@users.noreply.github.com>
2024-11-17 14:32:55 -0300

    increible sigue sin verse bien mis-datos.component

--- src/app/acerca-de-nosotros/acerca-de-nosotros.page.ts
+++ src/app/acerca-de-nosotros/acerca-de-nosotros.page.ts
@@ -1,19 +1,30 @@
+// src/app/acerca-de-nosotros/acerca-de-nosotros.page.ts
 import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../services/auth-service.service';  // Importar tu servicio
+import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
-
 @Component({
   selector: 'app-acerca-de-nosotros',
   templateUrl: './acerca-de-nosotros.page.html',
   styleUrls: ['./acerca-de-nosotros.page.scss'],
 })
 export class AcercaDeNosotrosPage implements OnInit {
+  userData: any = {};  // Datos del usuario
 
-  constructor(private menu: MenuController, private navController: NavController) { }
+  constructor(
+    private menu: MenuController,
+    private navController: NavController,
+    private authService: AuthServiceService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    console.log('AcercaDeNosotrosPage cargado');
+    this.loadUserData();  // Cargar los datos del usuario al iniciar la página
     this.menu.close("mainMenu");
   }
+
 
   goBack() {
     this.navController.pop();
@@ -23,4 +34,20 @@ export class AcercaDeNosotrosPage implements OnInit {
     this.menu.open('mainMenu');
   }
 
+  // Cargar los datos del usuario
+  async loadUserData() {
+    try {
+      const email = await this.authService.getActiveUserEmail();
+      console.log('Email del usuario activo:', email);  // Obtener el email del usuario activo
+      if (email) {
+        this.userData = await this.authService.getUserData(email);  // Obtener los datos del usuario por email
+        console.log('Datos del usuario cargados:', this.userData);
+      } else {
+        console.error('No hay usuario activo');
+        this.router.navigate(['/login']);  // Redirigir a login si no hay sesión activa
+      }
+    } catch (error) {
+      console.error('Error al cargar los datos del usuario', error);
+    }
+  }
 }
--- src/app/mis-datos/mis-datos.component.ts
+++ src/app/mis-datos/mis-datos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { AuthServiceService } from '../services/auth-service.service'; // Importar tu servicio
 import { Router } from '@angular/router';
 import { ChangeDetectorRef } from '@angular/core';
@@ -8,7 +8,7 @@ import { ChangeDetectorRef } from '@angular/core';
   templateUrl: './mis-datos.component.html',
   styleUrls: ['./mis-datos.component.scss'],
 })
-export class MisDatosComponent implements OnInit {
+export class MisDatosComponent implements OnInit, AfterViewChecked {
   userData: any = {};  // Datos del usuario
 
   constructor(
@@ -22,6 +22,14 @@ export class MisDatosComponent implements OnInit {
     this.loadUserData();  // Cargar los datos del usuario al iniciar el componente
   }
 
+  ngAfterViewChecked() {
+    // Este método se ejecuta después de cada ciclo de detección de cambios.
+    // Puedes usarlo para verificar si los datos están siendo cargados y si la vista se ha actualizado.
+    console.log('Vista después de la actualización: ', this.userData);
+    // Si los datos están correctos, podrías también forzar la detección de cambios, aunque no debería ser necesario.
+    // this.cdr.detectChanges();
+  }
+
   // Cargar los datos del usuario
   async loadUserData() {
     try {
@@ -29,8 +37,13 @@ export class MisDatosComponent implements OnInit {
       console.log('Email del usuario activo:', email);  // Obtener el email del usuario activo
       if (email) {
         this.userData = await this.authService.getUserData(email);  // Obtener los datos del usuario por email
+        delete this.userData.fecha_nacimiento;
+        console.log('Email del usuario activo:', email);
         console.log('Datos del usuario cargados:', JSON.stringify(this.userData));
-        this.cdr.detectChanges();
+        setTimeout(() => {
+          // Este timeout dará tiempo para que Angular procese los cambios
+          this.cdr.detectChanges();
+        }, 0);
       } else {
         console.error('No hay usuario activo');
         this.router.navigate(['/login']);  // Redirigir a login si no hay sesión activa
